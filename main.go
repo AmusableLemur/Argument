7e6e4eebc620b922b18537d1da52c67392b26ef8
Rasmus Larsson <rasmus@rasmuslarsson.se>
2020-06-13 01:15:33 +0200

    add templating

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+    "github.com/go-martini/martini"
+    "github.com/martini-contrib/render"
+)
 
 func main() {
-  m := martini.Classic()
-  m.Get("/", func() string {
-    return "Hello world!"
-  })
-  m.Run()
+    m := martini.Classic()
+
+    m.Use(render.Renderer(render.Options{
+        Extensions: []string{".html"},
+        Layout: "layout",
+    }))
+
+    m.Get("/", func(r render.Render) {
+        r.HTML(200, "index")
+    })
+
+    m.Run()
 }
