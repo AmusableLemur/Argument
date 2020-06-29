ce3cee0a13761fe2d31195809c8fd5ff00e54b81
Rasmus Larsson <rasmus@rasmuslarsson.se>
2020-06-29 22:00:07 +0200

    use configuration files

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+var config Config
+
 func setupRouter() *gin.Engine {
-    r := gin.Default()
-    r.LoadHTMLGlob("templates/*")
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
 
-    r.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.tmpl", gin.H{
-            "title": "Main website",
-        })
-    })
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": config.Title,
+		})
+	})
 
-    return r
+	return r
 }
 
 func main() {
+	stream, err := ioutil.ReadFile("config.toml")
+
+	if err != nil {
+		panic(err)
+	}
+
+	config = LoadConfig(stream)
+
 	setupRouter().Run(":8080")
 }
