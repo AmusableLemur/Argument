9a2e7e20d946f294c769ab4d5e9ed52ae27c8097
Rasmus Larsson <rasmus@rasmuslarsson.se>
2020-07-01 23:24:46 +0200

    move config tests to config test suite

--- main_test.go
+++ main_test.go
@@ -18,19 +18,3 @@ func TestIndexRoute(t *testing.T) {
 	assert.Equal(t, 200, w.Code)
 	assert.Contains(t, w.Body.String(), "New Post")
 }
-
-func TestConfigSetup(t *testing.T) {
-	config := setupConfig("config-sample.toml")
-
-	assert.Equal(t, "Argument", config.Title)
-}
-
-func TestFailedConfigSetup(t *testing.T) {
-	defer func() {
-		if r := recover(); r == nil {
-			t.Errorf("Missing config file should throw an error")
-		}
-	}()
-
-	setupConfig("non-existent-config.toml")
-}
