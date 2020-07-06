3485ed59139e81865af21bedd401fee42db0c879
Rasmus Larsson <rasmus@rasmuslarsson.se>
2020-07-06 22:10:08 +0200

    add methods for getting posts from db

---
+++ posts.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+var db *sql.DB
+
+// Post is a new thread on the discussion board
+type Post struct {
+	ID          int
+	Title       string
+	DateCreated string
+}
+
+// Connect is the function to prepare the database connection
+func Connect(URI string) {
+	var err error
+	db, err = sql.Open("mysql", config.Database.URI)
+
+	if err != nil {
+		log.Fatal("Unable to connect to database")
+	}
+}
+
+// GetPosts loads all the available posts from the database
+func GetPosts() []Post {
+	results, err := db.Query("SELECT id, title, created_at FROM posts")
+
+	if err != nil {
+		log.Fatal("Something is wrong with the database structure")
+	}
+
+	var posts []Post
+
+	for results.Next() {
+		var post Post
+
+		err = results.Scan(&post.ID, &post.Title, &post.DateCreated)
+
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		posts = append(posts, post)
+	}
+
+	return posts
+}
