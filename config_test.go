fa88c322fc4c6a21cff80e0a5fd40c379705cdf9
Rasmus Larsson <rasmus@rasmuslarsson.se>
2020-07-13 22:24:33 +0200

    change db host to ip

--- config_test.go
+++ config_test.go
@@ -37,5 +37,5 @@ func TestLoadingNonexistentConfig(t *testing.T) {
 func TestDatabaseURIConfig(t *testing.T) {
 	config := LoadConfig("config-sample.toml")
 
-	assert.Equal(t, "root:@tcp(localhost:3306)/argument", config.Database.URI)
+	assert.Equal(t, "root:@tcp(127.0.0.1:3306)/argument", config.Database.URI)
 }
