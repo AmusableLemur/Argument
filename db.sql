a2afa2367c90ea6e1a1327053a8c5ca60a1233f6
Rasmus Larsson <rasmus@rasmuslarsson.se>
2020-07-16 22:53:41 +0200

    add empty post to db

--- db.sql
+++ db.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `posts` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`title` VARCHAR(255) NOT NULL DEFAULT 'Untitled',
-	`created_at` INT NOT NULL,
+	`created_at` DATETIME NOT NULL,
 	`created_by` INT NOT NULL,
 	PRIMARY KEY (`id`)
 );
+
+INSERT INTO `posts` VALUES (1, 'Untitled', '2020-07-16 22:51:59', 1);
