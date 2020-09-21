dc7d6881c3b6edb74303172d1373cc3fcc469428
Rasmus Larsson <rasmus@rasmuslarsson.se>
2020-09-21 22:32:46 +0200

    add code to create users

--- internal/database/users.go
+++ internal/database/users.go
@@ -4,11 +4,23 @@ package database
 type User struct {
 	ID       int
 	Username string
-	Password string
+	Password string // Should be encrypted at all times
+}
+
+// CreateUser attempts to insert a new user to the database
+func CreateUser(user User) (int64, error) {
+	query := "INSERT INTO users (username, password) VALUES (?, ?)"
+	result, err := db.Exec(query, user.Username, user.Password)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return result.LastInsertId()
 }
 
 // GetUser finds a user by ID (if any) and returns it
-func GetUser(id int) (User, error) {
+func GetUser(id int64) (User, error) {
 	var user User
 
 	query := "SELECT id, username, password FROM users WHERE id = ?"
