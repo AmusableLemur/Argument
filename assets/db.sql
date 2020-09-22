a26b0d486ce54f5ef58f8765b4d6868addcf8747
Rasmus Larsson <rasmus@rasmuslarsson.se>
2020-09-22 18:10:18 +0200

    add users table

--- assets/db.sql
+++ assets/db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `posts` (
+CREATE TABLE IF NOT EXISTS `posts` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`title` VARCHAR(255) NOT NULL DEFAULT 'Untitled',
 	`created_at` DATETIME NOT NULL,
@@ -7,3 +7,10 @@ CREATE TABLE `posts` (
 );
 
 INSERT INTO `posts` VALUES (1, 'Untitled', '2020-07-16 22:51:59', 1);
+
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`username` varchar(50) NOT NULL,
+	`password` varchar(60) NOT NULL,
+	PRIMARY KEY (`id`)
+);
